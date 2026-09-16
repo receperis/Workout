e2dd6f7a699b60aae1543854302ca9263c8220ae
receperis <receperis@gmail.com>
2026-09-16 18:18:17 +0200

    fix: netlify update

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <WorkoutProvider>
         <App />
       </WorkoutProvider>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/workout',
+  base: '/',
   plugins: [tailwindcss(), react()],
 })
